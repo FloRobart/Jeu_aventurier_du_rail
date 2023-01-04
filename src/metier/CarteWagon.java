3da0d2c08c52290d92d69d1a107f86f1dc3d986b
Anaelle Mimifir <anaellemimi27@gmail.com>
2023-01-04 17:04:37 +0100

    update

--- src/metier/CarteWagon.java
+++ src/metier/CarteWagon.java
@@ -18,15 +18,17 @@ public class CarteWagon
             this.couleur    = couleur;
             this.joker = false;
         }
-        
+
         this.imageVerso = imageVerso;
     }
 
     public Color getCouleur() { return this.couleur; }
     public BufferedImage getImageVerso() { return this.imageVerso; }
+    public boolean isJoker() { return this.joker; }
 
     public void setCouleur(Color couleur) { this.couleur = couleur; }
     public void setImageVerso(BufferedImage imageVerso) { this.imageVerso = imageVerso; }
+    public void setJoker(boolean joker) { this.joker = joker; }
 
 
 
