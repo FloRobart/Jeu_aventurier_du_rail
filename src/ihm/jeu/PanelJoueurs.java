a01bd788e0c2bae893159aa11cd49bb82c615138
Anaelle Mimifir <anaellemimi27@gmail.com>
2023-01-05 15:00:08 +0100

    panel joueurs

--- src/ihm/jeu/PanelJoueurs.java
+++ src/ihm/jeu/PanelJoueurs.java
@@ -2,8 +2,13 @@ package ihm.jeu;
 
 import java.awt.Color;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
 
 import controleur.Controleur;
 
@@ -12,14 +17,49 @@ public class PanelJoueurs extends JPanel
 {
     private Controleur ctrl;
 
+    private JPanel      panelJoueurs;
+    private JScrollPane scrollJoueurs;
+    private JPanel[]    tabPanels;
+
 
     public PanelJoueurs(Controleur ctrl)
     {
         this.ctrl = ctrl;
 
 		this.setBackground(Color.YELLOW);
-		this.add(new JLabel("PanelJoueurs"));
+        this.setSize(500, 200);
 
+        this.panelJoueurs = new JPanel();
+        this.panelJoueurs.setBackground(new Color(40, 42, 54));
+        this.panelJoueurs.setLayout(new GridLayout(4, 1, 1, 1));
+
+        this.tabPanels = new JPanel[4];
+
+        for(int cpt=0; cpt< 4; cpt++)
+        {
+            tabPanels[cpt] = new JPanel();
+            tabPanels[cpt].setBackground(new Color(68, 71, 90));
+            tabPanels[cpt].setLayout(new BorderLayout(2,2));
+
+            JLabel lblNomJoueur = new JLabel("nom" + (cpt+1));
+                   lblNomJoueur.setForeground(Color.WHITE);
+            JLabel lblScoreJoueur = new JLabel("score" + (cpt+1));
+                   lblScoreJoueur.setForeground(Color.WHITE);
+            JLabel lblIcon = new JLabel();
+                   lblIcon.setIcon(new ImageIcon("./donnees/images/IconJoueur.png"));
+
+            tabPanels[cpt].add(lblNomJoueur, BorderLayout.NORTH);
+            tabPanels[cpt].add(lblIcon, BorderLayout.WEST);
+            tabPanels[cpt].add(lblScoreJoueur, BorderLayout.EAST);
+
+            this.panelJoueurs.add(tabPanels[cpt]);
+        }
+
+        this.scrollJoueurs = new JScrollPane(panelJoueurs, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        this.scrollJoueurs.setPreferredSize(new Dimension(200,200));
+
+        this.add(scrollJoueurs);
+ 
 		this.setVisible(true);
     }
 }
