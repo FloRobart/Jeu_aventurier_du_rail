666304b47835e1fdfe1e94098fb82c464a328301
Anaelle Mimifir <anaellemimi27@gmail.com>
2023-01-06 17:03:19 +0100

    update

--- src/ihm/jeu/PanelObjectif.java
+++ src/ihm/jeu/PanelObjectif.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.BorderLayout;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
