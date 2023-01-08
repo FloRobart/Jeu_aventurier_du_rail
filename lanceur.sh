ae7d49244db5f265b5f43c39e701c030ac021ff9
Floris <Floris.robart11@gmail.com>
2023-01-08 16:58:23 +0100

    update des lanceurs

--- lanceur.sh
+++ lanceur.sh
@@ -8,10 +8,10 @@ compilation()
     cp -r ./donnees/ ./bin/
 
     echo Compilation...
-    javac -encoding utf8 @compile.list && (java -cp "./bin:$CLASSPATH" controleur.Controleur && echo Fin de l\'execution. || echo -e "\nErreur d'EXECUTION.") || echo -e "\nErreur de COMPILATION."
+    javac -cp "$CLASSPATH:$CD\bin\donnees\jar_libraries\jdom-2.0.6.jar" -encoding utf8 -d "./bin" @compile.list && (java -cp "$CLASSPATH:$CD\bin:$CD\bin\donnees\jar_libraries\jdom-2.0.6.jar" controleur.Controleur && echo Fin de l\'execution. || echo -e "\nErreur d'EXECUTION.") || echo -e "\nErreur de COMPILATION."
 }
 
 
 export CLASSPATH="$CLASSPATH:./bin/donnees/jar_librairies/jdom-2.0.6.jar"
 echo Lancement de l\'application...
-java -cp "./bin:$CLASSPATH" controleur.Controleur && echo Fin de l\'execution. || compilation
+java -cp "$CLASSPATH:$CD\bin:$CD\bin\donnees\jar_libraries\jdom-2.0.6.jar" controleur.Controleur && echo Fin de l\'execution. || compilation
