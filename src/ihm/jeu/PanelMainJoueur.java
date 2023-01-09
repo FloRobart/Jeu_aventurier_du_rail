17956dd6009dac7c97ec7c278d3eb9cb3beb5477
Anaelle Mimifir <anaellemimi27@gmail.com>
2023-01-09 11:22:05 +0100

    update

--- src/ihm/jeu/PanelMainJoueur.java
+++ src/ihm/jeu/PanelMainJoueur.java
@@ -6,7 +6,6 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
