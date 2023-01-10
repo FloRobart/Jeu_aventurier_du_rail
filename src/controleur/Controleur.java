c57900bf72c6883c138d7bb45226d95840564ef6
DINH Tran Thai Duc <ducthai2001177@gmail.com>
2023-01-10 11:23:51 +0100

    Update- Update PanelAttente on btnRejoindre

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -285,7 +285,7 @@ public class Controleur
 	{
 
 		this.metier.creeClient(ip, true, password);
-
+		this.ihm.demarrerAttente(false);
 		return 1;
 		// this.joueur = new Joueur("Joueur 1");
 		// this.metier.ajouterJoueur(this.joueur);
