cfb5283ca7f515fae36337b9542b46646eb6a9f7
Nicolas Matuska <nicolas.matuska@gmail.com>
2023-01-10 17:18:56 +0100

    modif erreur

--- src/metier/partie/Partie.java
+++ src/metier/partie/Partie.java
@@ -171,8 +171,10 @@ public class Partie implements Serializable
 	public void arreterPartie()
 	{
 		//Recuperer les points, les noms
+		List<CarteObjectif >alCarteObjectif;
 		String[] tabNom = new String[this.joueurs.length];
-		for ( int cpt=0; cpt< this.joueurs.length; cpt++)
+		int scoreRetirer=0;
+		for ( int cptJoueur=0; cptJoueur< this.joueurs.length; cptJoueur++)
 		{
 			alCarteObjectif = this.joueurs[cptJoueur].getAlCartesObjectif();
 			for ( int cpt = 0; cpt < alCarteObjectif.size(); cpt++ )
@@ -180,7 +182,7 @@ public class Partie implements Serializable
 				scoreRetirer -= alCarteObjectif.get(cpt).getPoints();
 			}
 			this.joueurs[cptJoueur].ajouterScore(scoreRetirer);
-			
+
 			this.scoreFinal[cptJoueur] = this.joueurs[cptJoueur].getScore();
 			tabNom[cptJoueur] = this.joueurs[cptJoueur].getNom();
 		}
