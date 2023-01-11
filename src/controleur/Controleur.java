994cf45e1ca11bf5f881a42fce079a0a5a8a2fcb
Lucas Lemarchand <lucas.lemarchand2003@gmail.com>
2023-01-11 14:00:29 +0100

    fix selection arete

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -378,13 +378,6 @@ public class Controleur
 						}
 					}
 				}
-				else
-				{
-					if (this.areteSelectionnee != null)
-						this.ihm.afficherErreur("Aucune arête selectionné");
-					else
-						this.ihm.afficherErreur("Nombre de jeton insuffisant");
-				}
 			}
 
 			if (estValide)
@@ -414,6 +407,13 @@ public class Controleur
 				this.joueur.verifierObjectifs();
 				this.joueurSuivant();
 			}
+		}
+		else
+		{
+			if (this.areteSelectionnee == null)
+				this.ihm.afficherErreur("Aucune arête selectionné");
+			else
+				this.ihm.afficherErreur("Nombre de jeton insuffisant");
 		}
 	}
 
